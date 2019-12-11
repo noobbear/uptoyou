aa29ec5d1c467300ac9fa3f3ca40f48cdc1213e3
wfx <fx@fision.cn>
2019-12-11 10:34:20 +0800

    项目初始化

---
+++ src/main/java/cn/fision/uptoyou/UptoyouApplication.java
@@ -0,0 +1,13 @@
+package cn.fision.uptoyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UptoyouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UptoyouApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/fision/uptoyou/UptoyouApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.fision.uptoyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UptoyouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
